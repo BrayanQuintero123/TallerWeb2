4da030cb2f92b4afc55f4b4e403473f55ca0ee49
BrayanQuintero123 <brayan2221707@correo.uis.edu.co>
2024-05-21 18:11:30 -0500

    cambeo

--- Taller 7/mysite/mysite/urls.py
+++ Taller 7/mysite/mysite/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('',include('persona.urls')),
     path('admin/', admin.site.urls),
+    path('persona/',include('persona.urls')),
 ]
